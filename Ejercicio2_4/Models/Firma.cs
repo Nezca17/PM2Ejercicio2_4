2a4d130f0e3f274b9add8c8db6290054a1faf618
Nestor Zepeda <nestor.zepeda@outlook.com>
2023-07-19 10:45:02 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2_4/Models/Firma.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2_4.Models
+{
+    public class Firma
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string descrip { get; set; }
+        public byte[] sign { get; set; }
+    }
+}
